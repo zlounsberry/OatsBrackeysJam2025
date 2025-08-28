4497d04c0cb804b11af1e357dd4fdc7ad1a0b8c4
Zachary Lounsberry <zlounsberry@gmail.com>
2025-08-28 12:19:00 -0400

    fixed horrible faction animation bug

--- oatsbrackeysjam2025/scripts/faction_selection.gd
+++ oatsbrackeysjam2025/scripts/faction_selection.gd
@@ -2,6 +2,12 @@ extends NinePatchRect
 
 signal start_game
 
+const CC_UNPICKED_TEXT = "Choccy Chip"
+const SJ_UNPICKED_TEXT = "Strawbry Jammer"
+const SCC_UNPICKED_TEXT = "Sandwich Cookie Chan"
+const ALREADY_PICKED_TEXT = "ALREADY PICKED"
+
+
 @onready var current_selection: int = GameState.FACTIONS.SANDWICH_COOKIE
 @onready var left_position: Vector2 = $SCC.position
 @onready var right_position: Vector2 = $CC.position
@@ -11,19 +17,14 @@ signal start_game
 @onready var current_player_choosing: int = 0
 @onready var anim: AnimationPlayer = $AnimationPlayer
 @onready var confirm_button: Button = $Confirm
+@onready var scc_has_been_picked: bool = false
+@onready var cc_has_been_picked: bool = false
+@onready var sj_has_been_picked: bool = false
+@onready var can_select: bool = true
 
 
 func _ready() -> void:
-#	 Initialize player dictionary
-	#for value in range(GameState.number_of_players):
-		#GameState.current_player_dict[value]["faction_id"] = -99
-		#GameState.current_player_dict[value]["is_ai"] = false
-		#GameState.current_player_dict[value]["is_eliminated"] = false
 	print("current game dict: ", GameState.current_player_dict)
-	#is_animating = true
-	#anim.play("Select_SCC_left")
-	#await anim.animation_finished
-	#is_animating = false
 
 
 func _input(event: InputEvent) -> void:
@@ -31,47 +32,51 @@ func _input(event: InputEvent) -> void:
 		return
 	if event.is_action("ui_left"):
 		_scroll_left()
-	if event.is_action("ui_right"):
-		_scroll_right()
+	#if event.is_action("ui_right"):
+		#_scroll_right()
 
 
 func _scroll_left():
 	is_animating = true
+	prints("can_select", can_select)
+	can_select = true
 	match current_selection:
 		GameState.FACTIONS.SANDWICH_COOKIE:
 			anim.play("Select_CC_left")
+			prints("cc can_select", can_select, cc_has_been_picked)
+			if not cc_has_been_picked:
+				print('can select cc')
+				confirm_button.text = CC_UNPICKED_TEXT
+			else:
+				print('cannot select cc')
+				can_select = false
+				confirm_button.text = ALREADY_PICKED_TEXT
 			_update_current_selection(GameState.FACTIONS.CHOCCY_CHIP)
-			confirm_button.text = "CHOCCY CHIP"
-			#current_selection = GameState.FACTIONS.SANDWICH_COOKIE
+			prints("cc can_select", can_select, cc_has_been_picked)
 		GameState.FACTIONS.CHOCCY_CHIP:
 			anim.play("Select_SJ_left")
+			prints("sj can_select", can_select, sj_has_been_picked)
+			if not sj_has_been_picked:
+				print('can select sj')
+				confirm_button.text = SJ_UNPICKED_TEXT
+			else:
+				print('cannot select sj')
+				can_select = false
+				confirm_button.text = ALREADY_PICKED_TEXT
 			_update_current_selection(GameState.FACTIONS.STRAWBRY_JAMMER)
-			confirm_button.text = "STRAWBRY JAMMER"
-			#current_selection = GameState.FACTIONS.CHOCCY_CHIP
+			prints("sj can_select", can_select, sj_has_been_picked)
 		GameState.FACTIONS.STRAWBRY_JAMMER:
 			anim.play("Select_SCC_left")
+			prints("scc can_select", can_select, scc_has_been_picked)
+			if not scc_has_been_picked:
+				print('can select scc')
+				confirm_button.text = SCC_UNPICKED_TEXT
+			else:
+				print('cannot select scc')
+				can_select = false
+				confirm_button.text = ALREADY_PICKED_TEXT
 			_update_current_selection(GameState.FACTIONS.SANDWICH_COOKIE)
-			confirm_button.text = "SANDWICH_COOKIE"
-			#current_selection = GameState.FACTIONS.STRAWBRY_JAMMER
-	await anim.animation_finished
-	is_animating = false
-
-
-func _scroll_right():
-	is_animating = true
-	match current_selection:
-		GameState.FACTIONS.SANDWICH_COOKIE:
-			anim.play("Select_SJ_right")
-			_update_current_selection(GameState.FACTIONS.STRAWBRY_JAMMER)
-			confirm_button.text = "STRAWBRY JAMMER"
-		GameState.FACTIONS.CHOCCY_CHIP:
-			anim.play("Select_SCC_right")
-			_update_current_selection(GameState.FACTIONS.SANDWICH_COOKIE)
-			confirm_button.text = "SANDWICH COOKIE"
-		GameState.FACTIONS.STRAWBRY_JAMMER:
-			anim.play("Select_CC_right")
-			_update_current_selection(GameState.FACTIONS.CHOCCY_CHIP)
-			confirm_button.text = "CHOCCY CHIP"
+			prints("scc can_select", can_select, scc_has_been_picked)
 	await anim.animation_finished
 	is_animating = false
 
@@ -81,8 +86,21 @@ func _update_current_selection(faction_id: int) -> void:
 
 
 func _on_confirm_pressed() -> void:
+	if not can_select:
+		return
+	print("SELECTING ", current_selection)
 	GameState.current_player_dict[current_player_choosing]["faction_id"] = current_selection
 	GameState.current_player_dict[current_player_choosing]["is_ai"] = is_ai
+	match current_selection:
+		GameState.FACTIONS.SANDWICH_COOKIE:
+			scc_has_been_picked = true
+			$SCC.modulate = Color(200, 200, 200)
+		GameState.FACTIONS.CHOCCY_CHIP:
+			cc_has_been_picked = true
+			$CC.modulate = Color(200, 200, 200)
+		GameState.FACTIONS.STRAWBRY_JAMMER:
+			sj_has_been_picked = true
+			$SJ.modulate = Color(200, 200, 200)
 	current_player_choosing += 1
 	if current_player_choosing >= GameState.number_of_players:
 		start_game.emit()
